eba186b93118dc2c6b2d2d6dbdc1f663562ec0da
Juan Santa <jcsantahurtado@gmail.com>
2022-04-15 17:32:12 -0500

    Working with validations

--- assets/js/validations.js
+++ assets/js/validations.js
@@ -1,11 +1,9 @@
 export function valida(input) {
     const tipoDeInput = input.dataset.tipo;
-    console.log(tipoDeInput);
 
     if (input.validity.valid && input.checkValidity()) {
         input.parentElement.classList.remove("input-container--invalid");
         input.parentElement.querySelector(".input-message-error").innerHTML = "";
-        alert(`Mensaje enviado`)
     } else {
         input.parentElement.classList.add("input-container--invalid");
         input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
@@ -19,11 +17,34 @@ const tipoDeErrores = [
 ]
 
 const mensajesDeError = {
+    // Validación sección Contacto
     nombre: {
         valueMissing: "El campo nombre no puede estar vacío"
     },
     mensaje: {
         valueMissing: "El campo mensaje no puede estar vacío",
+    },
+
+    // Validación sección Login
+    email: {
+        valueMissing: "El campo correo no puede estar vacío",
+        typeMismatch: "El correo no es válido"
+    },
+    password: {
+        valueMissing: "El campo contraseña no puede estar vacío",
+        patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales"
+    },
+
+    // Validación sección Registro producto
+    nombreProducto: {
+        valueMissing: "El campo nombre producto no puede estar vacío"
+    },
+    precio: {
+        valueMissing: "El campo precio no puede estar vacío",
+        patternMismatch: "Al menos 3 dígitos, el campo precio del producto no debe contener letras"
+    },
+    descripcion: {
+        valueMissing: "El campo descripción no puede estar vacío"
     }
 }
 
@@ -31,6 +52,7 @@ const mensajesDeError = {
 function mostrarMensajeDeError(tipoDeInput, input) {
     let mensaje = "";
     tipoDeErrores.forEach(error => {
+
         if (input.validity[error]) {
             mensaje = mensajesDeError[tipoDeInput][error];
         }
